267ede3e11be6d8dfe918ae7023fe4dbaadbe448
octapereyra <octaviopereyra19@gmail.com>
2023-10-20 15:08:38 -0300

    fisrt commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+  fetch('https://api.quotable.io/random')
+    .then((response) => response.json())
+    .then((data) => {
+      const quoteText = document.querySelector('.quote-text')
+      const quoteAuthor = document.querySelector('.quote-author')
+      const tags = data.tags
+      quoteText.textContent = '"' + data.content + '"'
+      quoteAuthor.textContent = data.author
+      tags.map((tag) => {
+        const span = document.createElement('span')
+        span.className = 'tag'
+        span.textContent = tag
+        document.querySelector('.quote-tags').appendChild(span)
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
